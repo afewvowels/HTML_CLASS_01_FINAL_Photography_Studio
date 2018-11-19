ec451a6b274b764df3a9d9d520acdd5d996b2a33
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-11-19 08:42:15 -0500

    added comments

--- js/scripts_audio.js
+++ js/scripts_audio.js
@@ -1,8 +1,14 @@
+/*
+  This script handles the playback of the logo's audio element
+*/
+// Get the logo and the audio tag's contents...
 var elk = document.getElementById('logo');
 var sounds = document.getElementById('elk_bugle');
 
+// ...add the event listener for click event
 elk.addEventListener('click', playAudio);
 
+// ...define function for audio playback
 function playAudio() {
   sounds.play();
 }
